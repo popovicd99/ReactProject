ca5484a423c1fa7aefc3624287dc496524e9c7b0
popovicd99 <90621288+popovicd99@users.noreply.github.com>
2022-12-13 15:49:39 +0100

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,22 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
-import Background from '../img/background.jpg'
-import '../styles/Home.css'
+import Background from "../img/background.jpg";
+import "../styles/Home.css";
 
 function Home() {
   return (
-    <div className='home' style={{backgroundImage: `url(${Background})`}}>
-      <div className='header'>
+    <div className="home" style={{ backgroundImage: `url(${Background})` }}>
+      <div className="header">
         <h1>SNKR shop</h1>
-        <p>Just do it!</p>       
-        <div className='container'>
-          <Link to="/menu">
-          <div className='browse'>Browse</div>
+        <p>Just do it!</p>
+        <div className="container">
+          <Link to="/products">
+            <div className="browse">Browse</div>
           </Link>
         </div>
-        
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
